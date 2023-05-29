241ca59bf0f49a0555891c5fa7c60676cf3f467c
khelan2210 <134930412+khelan2210@users.noreply.github.com>
2023-05-29 16:07:28 +0530

    Add files via upload

---
+++ practicl1.cpp
@@ -0,0 +1,17 @@
+// wap to convert the temperature from celsius to fahrengeat//
+#include<iostream>
+using namespace std;
+
+int main()
+{
+float fah,cel;
+
+cout<<"enter celsius:";
+cin>> cel;
+
+fah=(cel*9/5)+32;
+
+
+cout<<"fah: \n"<<fah;
+
+}
